1a7be8fbd162b53ce3d5887b8e2234653ada5642
Ninaad Nirgudkar <ninaad.nirgudkar@razorpay.com>
2024-03-05 12:57:51 +0530

    push pop added to expression tree

--- dayone/expression_tree.go
+++ dayone/expression_tree.go
@@ -4,14 +4,24 @@ import "fmt"
 
 func Express() {
 	expression := "a+b-c"
+	expTree := new ExpressionTree{[]*Node{nil}}
 	for _, c := range expression {
 		str := string(c)
 		switch str {
 		case "+", "-":
+			//If a character is an operator pop two values
+			//from the stack make them its child
+			//and push the current node again.
 			fmt.Println("operator")
+			left := expTree.tree.pop()
+			right := expTree.tree.pop()
+			newNode:= Node{left: *left, right: *right, value: str}
+			expTree.push(newNode)
 		default:
 			fmt.Println("operand")
-
+			// If a character is an operand push that into the stack
+			newNode :=  Node{left: nil, right : nil, value: str}
+			expTree.push(newNode)
 		}
 	}
 }
@@ -20,15 +30,28 @@ type ExpressionTree struct {
 	tree []*Node
 }
 
-func (e *ExpressionTree) pop() {
+func (e *ExpressionTree) pop(node *Node) {
+	treeSize := len(e.tree)
+	if treeSize == 0 {
+		return
+	}
+	lastNode := e.tree[treeSize - 1]
+	for _, n := range e.tree {
 
+	}
 }
-func (e *ExpressionTree) push() {
-
+func (e *ExpressionTree) push(node *Node) {
+	treeSize := len(e.tree)
+	if treeSize != 0 {
+		lastNode := e.tree[treeSize - 1]
+		lastNode.right = node
+		node.left = lastNode
+	}
+	e.tree = append(e.tree, node)
 }
 
 type Node struct {
-	value []byte
+	value string
 	left  *Node
 	right *Node
 }
